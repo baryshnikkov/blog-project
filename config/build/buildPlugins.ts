429b1123fd008a1dc7378372f1e3ff640bbb381f
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-02-16 00:33:26 +0300

    настраиваем css modules

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,7 +1,7 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
-import path from "path";
 import {WebpackPluginInstance, ProgressPlugin} from "webpack";
 import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins({paths}: BuildOptions): WebpackPluginInstance[] {
 
@@ -9,6 +9,10 @@ export function buildPlugins({paths}: BuildOptions): WebpackPluginInstance[] {
     new HtmlWebpackPlugin({
       template: paths.html
     }),
-    new ProgressPlugin()
+    new ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
+    })
   ];
 };
