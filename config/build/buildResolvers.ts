f01f2197912ce8eb853c7c15dbcbb505aa620444
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-02-20 01:05:19 +0300

    настраиваем структуру по методологии feature-sliced design

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,6 +1,13 @@
-export function buildResolvers() {
+import {ResolveOptions} from "webpack";
+import {BuildOptions} from "./types/config";
+
+export function buildResolvers(options: BuildOptions): ResolveOptions {
 
   return {
     extensions: ['.tsx', '.ts', '.js'],
+    preferAbsolute: true,
+    modules: [options.paths.src, 'node_modules'],
+    mainFiles: ['index'],
+    alias: {}
   };
 };
