30e01ad2958ec04c40cecd1417ed459c4889be55
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-02-26 23:58:58 +0500

    настраиваем ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,30 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import { useEffect, useState } from 'react';
+
+interface BugButtonProps {
+  className?: string
+}
+
+export const BugButton = ({ className }: BugButtonProps) => {
+    const { t } = useTranslation('pageError');
+    const [error, setError] = useState(false);
+
+    const onThrowError = () => setError(true);
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+
+    return (
+        <Button
+            onClick={onThrowError}
+            className={classNames('', {}, [className])}
+        >
+            {t('Вызвыть ошибку')}
+        </Button>
+    );
+};
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,28 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string
+}
+
+const reloadPage = () => {
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+};
+
+export const PageError = ({ className }: PageErrorProps) => {
+    const { t } = useTranslation('pageError');
+
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            <p>{t('Произошла непредвиденная ошибка')}</p>
+            <Button
+                onClick={reloadPage}
+            >
+                {t('Обновить страницу')}
+            </Button>
+        </div>
+    );
+};
