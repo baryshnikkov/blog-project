06b2a1293ad8a139f807c5cf87755f821b58c6fd
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-03-17 22:33:32 +0300

    добавляем модальное окно, настраиваем Portal, изменяем NavBar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,15 +1,37 @@
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
 
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            /
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad blanditiis consequatur dignissimos esse
+                explicabo fuga hic ipsam iste iure mollitia nisi optio praesentium quasi quia quo reprehenderit vero
+                voluptas, voluptatibus!
+            </Modal>
         </div>
-    </div>
-);
+    );
+};
