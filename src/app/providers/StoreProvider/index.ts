fb0496a5dcfe0336debc693d2fc5866103928b9f
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-03-19 00:22:33 +0300

    настраиваем redux-toolkit

---
+++ src/app/providers/StoreProvider/index.ts
@@ -0,0 +1,9 @@
+import type { StateSchema } from './config/StateShema';
+import { createReduxStore } from './config/store';
+import { StoreProvider } from './ui/StoreProvider';
+
+export {
+    StoreProvider,
+    createReduxStore,
+    StateSchema,
+};
