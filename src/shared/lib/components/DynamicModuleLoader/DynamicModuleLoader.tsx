dfc6cf3086f06368c3637880746e3c6c9e812716
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-03-27 22:41:46 +0300

    настраиваем асинхронные редюсеры

---
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -0,0 +1,52 @@
+import { FC, ReactNode, useEffect } from 'react';
+import { useDispatch, useStore } from 'react-redux';
+import { ReduxStoreWithManager } from 'app/providers/StoreProvider';
+import { Reducer } from '@reduxjs/toolkit';
+import { StateSchemaKey } from 'app/providers/StoreProvider/config/StateShema';
+
+export type ReducerList = {
+    [name in StateSchemaKey]?: Reducer;
+}
+
+type ReducerListEntry = [StateSchemaKey, Reducer];
+
+interface DynamicModuleLoaderProps {
+  children: ReactNode;
+  reducers: ReducerList
+  removeAfterUnmount?: boolean;
+}
+
+export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
+    const {
+        children,
+        reducers,
+        removeAfterUnmount,
+    } = props;
+
+    const dispatch = useDispatch();
+    const store = useStore() as ReduxStoreWithManager;
+
+    useEffect(() => {
+        Object.entries(reducers).forEach(([name, reducer]: ReducerListEntry) => {
+            store.reducerManager.add(name, reducer);
+            dispatch({ type: `@INIT ${name} reducer` });
+        });
+
+        return () => {
+            if (removeAfterUnmount) {
+                Object.entries(reducers).forEach(([name]: ReducerListEntry) => {
+                    store.reducerManager.remove(name);
+                    dispatch({ type: `@DESTROY ${name} reducer` });
+                });
+            }
+        };
+        // eslint-disable-next-line
+    }, []);
+
+    return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+        <>
+            {children}
+        </>
+    );
+};
