b5026c375062393077155c61baf9618fb2ed6966
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-03-31 21:31:54 +0300

    настраиваем модуль профиля, включаем TS strict mode и исправляем ошибки

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,7 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
+        'no-undef': 'off',
         'i18next/no-literal-string': ['error', {
             markupOnly: true,
             ignoreAttribute: ['data-testid', 'to'],
