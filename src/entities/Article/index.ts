3132d3dae040de0b079a31e7946414afb4ff769b
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-04-18 13:33:22 +0300

    настраиваем fetching articles, view type selector

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,7 +1,8 @@
 import { ArticleDetails } from 'entities/Article/ui/ArticleDetails/ArticleDetails';
 import { getArticleDetailsData } from './model/selectors/getArticleDetailsData/getArticleDetailsData';
-import { ArticleDetailsSchema, ArticleView } from './model/types/article';
+import { Article, ArticleDetailsSchema, ArticleView } from './model/types/article';
 import { ArticleList } from './ui/ArticleList/ArticleList';
+import { ArticleViewSelector } from './ui/ArticleViewSelector/ArticleViewSelector';
 
 export {
     ArticleDetails,
@@ -9,4 +10,6 @@ export {
     getArticleDetailsData,
     ArticleView,
     ArticleList,
+    Article,
+    ArticleViewSelector,
 };
--- src/pages/ArticlesPage/index.ts
+++ src/pages/ArticlesPage/index.ts
@@ -1,3 +1,7 @@
+import { ArticlesPageSchema } from './model/types/articlesPageSchema';
+import { ArticlesPageAsync as ArticlesPage } from './ui/ArticlesPage/ArticlesPage.async';
+
 export {
-    ArticlesPageAsync as ArticlesPage,
-} from './ui/ArticlesPage/ArticlesPage.async';
+    ArticlesPageSchema,
+    ArticlesPage,
+};
