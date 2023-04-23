c2954f401c7a19f82ea469b00a69f518d81dcf66
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-04-23 12:08:00 +0300

    настраиваем CopyPlugin, подготовка к продакшну

--- src/pages/NotFoundPage/ui/NotFoundPage.async.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.async.tsx
@@ -1,8 +1,3 @@
 import { lazy } from 'react';
 
-// export const NotFoundPageAsync = lazy(() => import('./MainPage'));
-
-export const NotFoundPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    setTimeout(() => resolve(import('./NotFoundPage')), 1500);
-}));
+export const NotFoundPageAsync = lazy(() => import('./NotFoundPage'));
