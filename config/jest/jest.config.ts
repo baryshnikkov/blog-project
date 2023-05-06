78c1f6f5aad7b0063a69c4081b29633d2d35d1f8
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-05-06 20:47:32 +0300

    настраиваем jest-html-reporters

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -69,6 +69,16 @@ export default {
         __PROJECT__: 'jest',
     },
 
+    // Use this configuration option to add custom reporters to Jest
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+        }],
+    ],
+
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
@@ -127,9 +137,6 @@ export default {
 
     // Run tests from one or more projects
     // projects: undefined,
-
-    // Use this configuration option to add custom reporters to Jest
-    // reporters: undefined,
 
     // Automatically reset mock state before every test
     // resetMocks: false,
