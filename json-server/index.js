62452560fe78c0824a8edc1ebc41ae7b5adfb63f
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-05-23 17:54:56 +0300

    настраиваем nginx, nginx gzip, pm2, https, проксирование запросов, скрипт для деплоя

--- json-server/index.js
+++ json-server/index.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
+const https = require('https');
+const http = require('http');
+
+const options = {
+    key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+};
 
 const server = jsonServer.create();
 
@@ -21,7 +28,9 @@ server.use(async (req, res, next) => {
 server.post('/login', (req, res) => {
     try {
         const { username, password } = req.body;
-        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const db = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'),
+        );
         const { users = [] } = db;
 
         const userFromBd = users.find(
@@ -52,6 +61,16 @@ server.use((req, res, next) => {
 server.use(router);
 
 // запуск сервера
-server.listen(8000, () => {
-    console.log('server is running on 8000 port');
+const PORT = 8443;
+const HTTP_PORT = 8000;
+
+const httpsServer = https.createServer(options, server);
+const httpServer = http.createServer(server);
+
+httpsServer.listen(PORT, () => {
+    console.log(`server is running on ${PORT} port`);
+});
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`server is running on ${HTTP_PORT} port`);
 });
