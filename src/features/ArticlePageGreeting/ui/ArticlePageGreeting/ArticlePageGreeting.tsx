350f9e41cc75bf997601f872ec3426bdba3a2da4
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-05-26 12:41:35 +0300

    практическое применение json settings, работа с новыми пользователями

---
+++ src/features/ArticlePageGreeting/ui/ArticlePageGreeting/ArticlePageGreeting.tsx
@@ -0,0 +1,65 @@
+import { useTranslation } from 'react-i18next';
+import { memo, useCallback, useEffect, useState } from 'react';
+import { isMobile } from 'react-device-detect';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { Modal } from '@/shared/ui/Modal';
+import { saveJsonSettings, useJsonSettings } from '@/entities/User';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { Text } from '@/shared/ui/Text';
+import { Drawer } from '@/shared/ui/Drawer';
+
+interface ArticlePageGreetingProps {
+    className?: string;
+}
+
+export const ArticlePageGreeting = memo((props: ArticlePageGreetingProps) => {
+    const { className } = props;
+    const { t } = useTranslation('articlePageGreeting');
+    const [isOpen, setIsOpen] = useState(false);
+    const { articlesPageWasOpen } = useJsonSettings();
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (!articlesPageWasOpen) {
+            setIsOpen(true);
+            dispatch(saveJsonSettings({ articlesPageWasOpen: true }));
+        }
+    }, [articlesPageWasOpen, dispatch]);
+
+    const onClose = useCallback(() => {
+        setIsOpen(false);
+    }, []);
+
+    const text = (
+        <Text
+            title={t('Добро пожаловать на страницу статей')}
+            text={t(
+                'Здесь вы можете искать и просматривать статьи на различные темы',
+            )}
+        />
+    );
+
+    if (isMobile) {
+        return (
+            <Drawer
+                className={classNames('', {}, [className])}
+                lazy
+                isOpen={isOpen}
+                onClose={onClose}
+            >
+                {text}
+            </Drawer>
+        );
+    }
+
+    return (
+        <Modal
+            className={classNames('', {}, [className])}
+            lazy
+            isOpen={isOpen}
+            onClose={onClose}
+        >
+            {text}
+        </Modal>
+    );
+});
